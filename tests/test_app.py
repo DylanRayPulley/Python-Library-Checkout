5f145d3795fca8e4e6a04cc00ca36966f497d6ae
DylanRayPulley <d.pulley2@students.clark.edu>
2025-12-04 17:32:11 -0800

    Update test_app.py again again

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,9 +8,11 @@ def setup_sample():
   cat.add(Book(Id("B1"), "Rust for Humans"))
   cat.add(Book(Id("B2"), "Pythonic Patterns"))
   cat.add(Dvd(Id("D1"), "Taking Flight"))
-return cat
-  def test_add_and_get():
+  return cat
+
+def test_add_and_get():
   cat = setup_sample()
   assert cat.get("B1").title == "Rust for Humans"
   assert cat.get("D1").days_allowed() == 7
+
 
