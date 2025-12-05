b00ca5c1b91e3c3c4321863e2ff1708c28df1ff7
DylanRayPulley <d.pulley2@students.clark.edu>
2025-12-04 17:31:50 -0800

    Update test_app.py again

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,13 +2,15 @@ import pytest
 from app.v1.items import Id, Book, Dvd
 from app.v1.catalog import Catalog
 from app.v1.member import Member
+
 def setup_sample():
-cat = Catalog()
-cat.add(Book(Id("B1"), "Rust for Humans"))
-cat.add(Book(Id("B2"), "Pythonic Patterns"))
-cat.add(Dvd(Id("D1"), "Taking Flight"))
+  cat = Catalog()
+  cat.add(Book(Id("B1"), "Rust for Humans"))
+  cat.add(Book(Id("B2"), "Pythonic Patterns"))
+  cat.add(Dvd(Id("D1"), "Taking Flight"))
 return cat
-def test_add_and_get():
-cat = setup_sample()
-assert cat.get("B1").title == "Rust for Humans"
-assert cat.get("D1").days_allowed() == 7
+  def test_add_and_get():
+  cat = setup_sample()
+  assert cat.get("B1").title == "Rust for Humans"
+  assert cat.get("D1").days_allowed() == 7
+
